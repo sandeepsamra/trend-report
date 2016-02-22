53b7b232cb3d611e0956ce8d1dc76ff2721e5eb1
Sandeep Samra <ssamra67@gmail.com>
2016-02-22 11:25:06 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TrendReport::Application.config.secret_key_base = '5613d7d758239206554adf70750d91a4943fb608b396a368afa24b65d178a6dcf5c292b8affe998586ed1f409abe68ab1ae3abda91f4aa306b4df94fcc0b0ea7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TrendReport::Application.config.session_store :cookie_store, key: '_trend-report_session'

e68c873d3368487233f72392cff2fa717a614d06
Sandeep Samra <ssamra67@gmail.com>
2016-02-22 20:26:30 -0500

    scraped 5 sources

--- app/controllers/fashion/articles_controller.rb
+++ app/controllers/fashion/articles_controller.rb
@@ -17,16 +17,6 @@ class Fashion::ArticlesController < ApplicationController
     #figure out how to keep images and urls together in same object
     #loop through each div, and create new Article object (attributes = elements in div)
     #background worker to scrape at regular intervals?
-
-    #page = Nokogiri::HTML(open("http://www.whowhatwear.com/section/fashion-trends"))
-
-    # @page.css('article.promo.promo-feed.wow.promoIn').each do |article|
-    #   article.css('div.promo-feed-img img').attr('src')
-    #   article.css('div.promo-feed-headline a h3').text
-    #   url = "http://www.whowhatwear.com"
-    #   "#{url}#{article.css('div.promo-feed-headline a')[1].attr('href')}"
-    # end
-
   end
 
 end

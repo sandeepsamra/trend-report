ee49b813173de4e031b4bf2af0af642292504da8
Sandeep Samra <ssamra67@gmail.com>
2016-02-23 19:35:39 -0500

    updated FashionScraper class

--- lib/fashion_scraper.rb
+++ lib/fashion_scraper.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class FashionScraper
 
   def scrape_www
@@ -7,23 +9,23 @@ class FashionScraper
       title = article.css('div.promo-feed-headline a h3').text
       url = "http://www.whowhatwear.com"
       link = "#{url}#{article.css('div.promo-feed-headline a')[1].attr('href')}"
-      Article.create (
-        url: url,
+      Article.create(
+        url: link,
         image: image,
         title: title
       )
     end
   end
 
-  def scrape_Refinery_29
+  def scrape_refinery_29
     refinery = Nokogiri::HTML(open("http://www.refinery29.com/trends"))
     refinery.css('article.collection-story-container').each do |article|  
       image = article.css('figure.collection-story-container-figure a.ga-trackable img').attr('src')
       title = article.css('div.collection-story-details h2.collection-story-headline.ga-trackable a').text
       url = "http://www.refinery29.com"
       link = "#{url}#{article.css('div.collection-story-details h2.collection-story-headline.ga-trackable a').attr('href')}"
-      Article.create (
-        url: url,
+      Article.create(
+        url: link,
         image: image,
         title: title
       )
@@ -32,13 +34,13 @@ class FashionScraper
 
   def scrape_elle
     elle = Nokogiri::HTML(open("http://www.ellecanada.com/fashion/trends"))
-    sections = @elle.css('div#primary ul.subchannel-listing li.first.photo') && @elle.css('div#primary ul.subchannel-listing li.photo')
+    sections = elle.css('div#primary ul.subchannel-listing li.first.photo') && elle.css('div#primary ul.subchannel-listing li.photo')
     sections.each do |article|
       image = article.css('div.bloc div.thumb a img').attr('src')
       title = article.css('div.bloc h3 a').text
       link = article.css('div.bloc h3 a').attr('href')
-      Article.create (
-        url: url,
+      Article.create(
+        url: link,
         image: image,
         title: title
       )
@@ -51,8 +53,8 @@ class FashionScraper
       image = article.css('a.category-article-thumb img').attr('src')
       title = article.css('div.category-article-main header h2 a.post-title').text
       link = article.css('div.category-article-main header h2 a.post-title').attr('href')
-      Article.create (
-        url: url,
+      Article.create(
+        url: link,
         image: image,
         title: title
       )
@@ -66,8 +68,8 @@ class FashionScraper
       image = "#{url}#{article.css('div.g-search-content div.g-search-media a img').attr('src')}"
       title = article.css('div.g-search-content div.g-search-body a span').text
       link = "#{url}#{article.css('div.g-search-content div.g-search-body a.g-title').attr('href')}"
-      Article.create (
-        url: url,
+      Article.create(
+        url: link,
         image: image,
         title: title
       )

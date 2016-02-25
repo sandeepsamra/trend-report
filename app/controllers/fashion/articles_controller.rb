e849e5cd6bb1fc7e3f554aae180638b1117a06b7
nfamula <nfamula@gmail.com>
2016-02-25 22:09:52 +0000

    parse url and title

--- app/controllers/fashion/articles_controller.rb
+++ app/controllers/fashion/articles_controller.rb
@@ -1,12 +1,15 @@
 class Fashion::ArticlesController < ApplicationController
 
   def index
-    @article = Article.where(id: 747)
+    @article = Article.all
     #render json: @article, root: false
   end
 
-  def create
+  # def show_womens
+  # 	@article = Article.where(category: "wfashion")
+  # end
 
-  end
-
+  # def show_mens
+  # 	@article = Article.where(category: "mfashion")
+  # end  
 end

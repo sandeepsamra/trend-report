64842033dff51ded57b5fdb7f34427efb8f16939
Sandeep Samra <ssamra67@gmail.com>
2016-02-25 18:38:30 -0500

    fixed routes and controllers

---
+++ app/controllers/fashion/women/articles_controller.rb
@@ -0,0 +1,12 @@
+class Fashion::Women::ArticlesController < ApplicationController
+
+  def index
+    #@article = Article.where(id: 747)
+    #render json: @article, root: false
+  end
+
+  def create
+
+  end
+
+end

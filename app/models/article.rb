c18085af84d665589dee26d7e3853cd49f18379c
nfamula <nfamula@gmail.com>
2016-02-29 21:30:51 +0000

    search route fixed

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,7 +7,6 @@ class Article < ActiveRecord::Base
   validates :image, uniqueness: true
 
    def self.search(query)
-    # where(:title, query) -> This would return an exact match of the query
     where("title like ?", "%#{query}%") 
   end
 

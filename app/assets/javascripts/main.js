9f3d18891935ead9801e96cb6549690bf2d9d53b
nfamula <nfamula@gmail.com>
2016-03-01 05:09:07 +0000

    search button responsive

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,263 +1,261 @@
-$(window).load(function(){
-	$('#preloader').fadeOut('slow',function(){$(this).remove();});
+$(window).load(function() {
+    $('#preloader').fadeOut('slow', function() { $(this).remove(); });
 });
 
 /******************************************************************************************************************************
 Menu
-*******************************************************************************************************************************/ 
+*******************************************************************************************************************************/
 
 $(document).ready(function() {
 
-	var bodyEl = document.body,
-		//content = document.querySelector( '.content-wrap' ),
-		openbtn = document.getElementById( 'open-button' ),
-		closebtn = document.getElementById( 'close-button' ),
-		isOpen = false;
+    var bodyEl = document.body,
+        //content = document.querySelector( '.content-wrap' ),
+        openbtn = document.getElementById('open-button'),
+        closebtn = document.getElementById('close-button'),
+        isOpen = false;
 
-	function init() {
-		initEvents();
-	}
+    function init() {
+        initEvents();
+    }
 
-	function initEvents() {
-		openbtn.addEventListener( 'click', toggleMenu );
-		if( closebtn ) {
-			closebtn.addEventListener( 'click', toggleMenu );
-		}
+    function initEvents() {
+        openbtn.addEventListener('click', toggleMenu);
+        if (closebtn) {
+            closebtn.addEventListener('click', toggleMenu);
+        }
 
-		/* close the menu element if the target it´s not the menu element or one of its descendants..
-		content.addEventListener( 'click', function(ev) {
-			var target = ev.target;
-			if( isOpen && target !== openbtn ) {
-				toggleMenu();
-			}
-		} );
-		*/
-	}
+        /* close the menu element if the target it´s not the menu element or one of its descendants..
+        content.addEventListener( 'click', function(ev) {
+        	var target = ev.target;
+        	if( isOpen && target !== openbtn ) {
+        		toggleMenu();
+        	}
+        } );
+        */
+    }
 
-	function toggleMenu() {
-		if( isOpen ) {
-			classie.remove( bodyEl, 'show-menu' );
-		}
-		else {
-			classie.add( bodyEl, 'show-menu' );
-		}
-		isOpen = !isOpen;
-	}
+    function toggleMenu() {
+        if (isOpen) {
+            classie.remove(bodyEl, 'show-menu');
+        } else {
+            classie.add(bodyEl, 'show-menu');
+        }
+        isOpen = !isOpen;
+    }
 
-	init();
+    init();
 
-	//Dropdown menu
+    //Dropdown menu
 
-	$('#nav-link-click').on('mouseover', function() {
-    $('ul#hide-link').removeClass('hide-link');
-  });
+    $('#nav-link-click').on('mouseover', function() {
+        $('ul#hide-link').removeClass('hide-link');
+    });
 
-   $('#nav-link-click').on('mouseout', function() {
-    $('ul#hide-link').addClass('hide-link');
-  });
+    $('#nav-link-click').on('mouseout', function() {
+        $('ul#hide-link').addClass('hide-link');
+    });
 
-  $('li.sublink').on('mouseover', function() {
-    $('ul#sub-hide-link').removeClass('sub-hide-link');
-  });
+    $('li.sublink').on('mouseover', function() {
+        $('ul#sub-hide-link').removeClass('sub-hide-link');
+    });
 
-  $('li.sublink').on('mouseout', function() {
-    $('ul#sub-hide-link').addClass('sub-hide-link');
-  });
+    $('li.sublink').on('mouseout', function() {
+        $('ul#sub-hide-link').addClass('sub-hide-link');
+    });
 
 });
 
 /******************************************************************************************************************************
 Log in & sign up forms
-*******************************************************************************************************************************/ 
+*******************************************************************************************************************************/
 
 $(document).ready(function() {
 
-//Switch between forms on landing page
+    //Switch between forms on landing page
 
-	$('#log-in-click').on('click', function() {
-		$('#sign-up-form').addClass('hide-form');
-		$('#sign-in-form').removeClass('hide-form');
-	});
-
-	$('#sign-up-click').on('click', function() {
-		$('#sign-in-form').addClass('hide-form');
-		$('#sign-up-form').removeClass('hide-form');
-	});
-
-//Sign up form validations
-
-	$(function() {
-    $('#sign-up-form').submit(function(event) {
-    	$('#form-group-fname').removeClass('has-error');
-    	$('#form-group-lname').removeClass('has-error');
-    	$('#form-group-email').removeClass('has-error');
-    	$('#form-group-password').removeClass('has-error');
-    	$('#form-group-pconfirm').removeClass('has-error');
-      var errors = [];
-      if ($('#form-fname').val().length <= 0){
-        errors.push("You didn't enter a first name.");
-        $('#form-group-fname').addClass('has-error');
-      };
-
-      if ($('#form-lname').val().length <= 0){
-        errors.push("You didn't enter a last name.");
-        $('#form-group-lname').addClass('has-error');
-      };
-
-      if ($('#form-email').val().indexOf('@') === -1){
-        errors.push("Your email is not the right format.");
-        $('#form-group-email').addClass('has-error');
-      };
-
-			if ($('#form-password').val().length < 6){
-        errors.push("Your password must be at least 6 characters long.");
-        $('#form-group-password').addClass('has-error');
-      };
-
-      if ($('#form-pconfirm').val() !== $('#form-password').val()){
-        errors.push("You didn't confirm your password.");
-        $('#form-group-pconfirm').addClass('has-error');
-      };
-
-      if (errors.length > 0) {
-      	event.preventDefault();
-      	$('div#sign-up-errors').children("h4").remove();
-      	$('div#sign-up-errors').append('<h4>' + 'ERRORS: ' + '</h4>');
-      	for (i = 0; i < errors.length; i += 1) {
-      		$('div#sign-up-errors').append('<h4>' + errors[i] + '</h4>');
-      	}
-      }
+    $('#log-in-click').on('click', function() {
+        $('#sign-up-form').addClass('hide-form');
+        $('#sign-in-form').removeClass('hide-form');
     });
-  });
+
+    $('#sign-up-click').on('click', function() {
+        $('#sign-in-form').addClass('hide-form');
+        $('#sign-up-form').removeClass('hide-form');
+    });
+
+    //Sign up form validations
+
+    $(function() {
+        $('#sign-up-form').submit(function(event) {
+            $('#form-group-fname').removeClass('has-error');
+            $('#form-group-lname').removeClass('has-error');
+            $('#form-group-email').removeClass('has-error');
+            $('#form-group-password').removeClass('has-error');
+            $('#form-group-pconfirm').removeClass('has-error');
+            var errors = [];
+            if ($('#form-fname').val().length <= 0) {
+                errors.push("You didn't enter a first name.");
+                $('#form-group-fname').addClass('has-error');
+            };
+
+            if ($('#form-lname').val().length <= 0) {
+                errors.push("You didn't enter a last name.");
+                $('#form-group-lname').addClass('has-error');
+            };
+
+            if ($('#form-email').val().indexOf('@') === -1) {
+                errors.push("Your email is not the right format.");
+                $('#form-group-email').addClass('has-error');
+            };
+
+            if ($('#form-password').val().length < 6) {
+                errors.push("Your password must be at least 6 characters long.");
+                $('#form-group-password').addClass('has-error');
+            };
+
+            if ($('#form-pconfirm').val() !== $('#form-password').val()) {
+                errors.push("You didn't confirm your password.");
+                $('#form-group-pconfirm').addClass('has-error');
+            };
+
+            if (errors.length > 0) {
+                event.preventDefault();
+                $('div#sign-up-errors').children("h4").remove();
+                $('div#sign-up-errors').append('<h4>' + 'ERRORS: ' + '</h4>');
+                for (i = 0; i < errors.length; i += 1) {
+                    $('div#sign-up-errors').append('<h4>' + errors[i] + '</h4>');
+                }
+            }
+        });
+    });
 
 });
 
 /******************************************************************************************************************************
 Masonry Grid
-*******************************************************************************************************************************/ 
+*******************************************************************************************************************************/
 
 
-$(document).ready(function(){
-  $(function(){
-    var $container = jQuery('#masonry-grid').masonry({
-      itemSelector: '.grid-item'
+$(document).ready(function() {
+    $(function() {
+        var $container = jQuery('#masonry-grid').masonry({
+            itemSelector: '.grid-item'
+        });
+        $container.imagesLoaded().progress(function() {
+            $container.masonry('layout');
+        });
     });
-    $container.imagesLoaded().progress( function() {
-      $container.masonry('layout');
-    });
-  });
 });
 
 /******************************************************************************************************************************
 Masonry Infinite Scroll
-*******************************************************************************************************************************/ 
-$(document).ready(function(){
-  var $container = jQuery('#masonry-grid').masonry;
+*******************************************************************************************************************************/
+$(document).ready(function() {
+    var $container = jQuery('#masonry-grid').masonry;
 
-$container.imagesLoaded(function(){
-  $container.masonry({
-    itemSelector: '.grid-item',
-    columnWidth: 100
-  });
-});
-
-$container.infinitescroll({
-  navSelector  : '#infinite-scrolling .page_current',    // selector for the paged navigation 
-  nextSelector : '#infinite-scrolling .next a',  // selector for the NEXT link (to page 2)
-  itemSelector : '.grid-item',     // selector for all items you'll retrieve
-  loading: {
-      finishedMsg: 'No more trends to load.',
-    }
-  },
-  // trigger Masonry as a callback
-  function( newElements ) {
-    // hide new items while they are loading
-    var $newElems = $( newElements ).css({ opacity: 0 });
-    // ensure that images load before adding to masonry layout
-    $newElems.imagesLoaded(function(){
-      // show elems now they're ready
-      $newElems.animate({ opacity: 1 });
-      $container.masonry( 'appended', $newElems, true ); 
+    $container.imagesLoaded(function() {
+        $container.masonry({
+            itemSelector: '.grid-item',
+            columnWidth: 100
+        });
     });
-  }
-);
+
+    $container.infinitescroll({
+            navSelector: '#infinite-scrolling .page_current', // selector for the paged navigation 
+            nextSelector: '#infinite-scrolling .next a', // selector for the NEXT link (to page 2)
+            itemSelector: '.grid-item', // selector for all items you'll retrieve
+            loading: {
+                finishedMsg: 'No more trends to load.',
+            }
+        },
+        // trigger Masonry as a callback
+        function(newElements) {
+            // hide new items while they are loading
+            var $newElems = $(newElements).css({ opacity: 0 });
+            // ensure that images load before adding to masonry layout
+            $newElems.imagesLoaded(function() {
+                // show elems now they're ready
+                $newElems.animate({ opacity: 1 });
+                $container.masonry('appended', $newElems, true);
+            });
+        }
+    );
 });
 /******************************************************************************************************************************
 Waypoints
-*******************************************************************************************************************************/ 
+*******************************************************************************************************************************/
 
 $(document).ready(function() {
 
-	$('.wp1').waypoint(function() {
-		$('.wp1').addClass('animated fadeInLeft');
-	}, {
-		offset: '75%'
-	});
-	$('.wp2').waypoint(function() {
-		$('.wp2').addClass('animated fadeInUp');
-	}, {
-		offset: '75%'
-	});
-	$('.wp3').waypoint(function() {
-		$('.wp3').addClass('animated fadeInDown');
-	}, {
-		offset: '55%'
-	});
-	$('.wp4').waypoint(function() {
-		$('.wp4').addClass('animated fadeInDown');
-	}, {
-		offset: '75%'
-	});
-	$('.wp5').waypoint(function() {
-		$('.wp5').addClass('animated fadeInUp');
-	}, {
-		offset: '75%'
-	});
-	$('.wp6').waypoint(function() {
-		$('.wp6').addClass('animated fadeInDown');
-	}, {
-		offset: '75%'
-	});
+    $('.wp1').waypoint(function() {
+        $('.wp1').addClass('animated fadeInLeft');
+    }, {
+        offset: '75%'
+    });
+    $('.wp2').waypoint(function() {
+        $('.wp2').addClass('animated fadeInUp');
+    }, {
+        offset: '75%'
+    });
+    $('.wp3').waypoint(function() {
+        $('.wp3').addClass('animated fadeInDown');
+    }, {
+        offset: '55%'
+    });
+    $('.wp4').waypoint(function() {
+        $('.wp4').addClass('animated fadeInDown');
+    }, {
+        offset: '75%'
+    });
+    $('.wp5').waypoint(function() {
+        $('.wp5').addClass('animated fadeInUp');
+    }, {
+        offset: '75%'
+    });
+    $('.wp6').waypoint(function() {
+        $('.wp6').addClass('animated fadeInDown');
+    }, {
+        offset: '75%'
+    });
 
 });
 
 /******************************************************************************************************************************
 Nav Button
-*******************************************************************************************************************************/ 
+*******************************************************************************************************************************/
 
 $(window).load(function() {
 
-	$('.nav_slide_button').click(function() {
-		$('.pull').slideToggle();
-	});
+    $('.nav_slide_button').click(function() {
+        $('.pull').slideToggle();
+    });
 
 });
 
 
 $(function() {
 
-	$('a[href*=#]:not([href=#])').click(function() {
-		if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
 
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-			if (target.length) {
-				$('html,body').animate({
-					scrollTop: target.offset().top - 125
-				}, 1000);
-				return false;
-			}
-		}
-	});
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top - 125
+                }, 1000);
+                return false;
+            }
+        }
+    });
 
 });
 
 /******************************************************************************************************************************
 Nav Transform
-*******************************************************************************************************************************/ 
-$(document).ready(function(){
-  document.querySelector("#open-button").addEventListener("click", function() {
-  	this.classList.toggle("active");
-  });
+*******************************************************************************************************************************/
+$(document).ready(function() {
+    document.querySelector("#open-button").addEventListener("click", function() {
+        this.classList.toggle("active");
+    });
 });
-

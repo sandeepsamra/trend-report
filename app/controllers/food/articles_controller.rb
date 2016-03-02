d70c94abb5e625e00280a6f66b665995ef7344b1
nfamula <nfamula@gmail.com>
2016-03-02 02:09:37 +0000

    favourites search bar

--- app/controllers/food/articles_controller.rb
+++ app/controllers/food/articles_controller.rb
@@ -2,7 +2,7 @@ class Food::ArticlesController < ApplicationController
 
   def index
     if params[:query]
-      @article = Article.search(params[:query]).where(category: "food")
+      @article = Article.search(params[:query]).where(category: "food").page(params[:page])
     else
       @article = Article.where(category: "food").page(params[:page])
     end

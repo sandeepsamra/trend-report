b9fd443a6fac16c71d3cd14262292efc8cc4cfce
Sandeep Samra <ssamra67@gmail.com>
2016-03-02 16:08:46 -0500

    fixed dropdown menu

--- app/assets/javascripts/favourites.js
+++ app/assets/javascripts/favourites.js
@@ -16,6 +16,7 @@ $(document).ready(function(){
       }
     }).done(function(data) {
       if( data.ok ) {
+        link.parents('.article').append('<h4>Already saved!</h4>');
         link.remove();
       }
     });

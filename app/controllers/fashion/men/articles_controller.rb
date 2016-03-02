2af687b141f2a870ebc19971d9e8111382989aa7
nfamula <nfamula@gmail.com>
2016-03-02 17:12:22 +0000

    added flash messages

--- app/controllers/fashion/men/articles_controller.rb
+++ app/controllers/fashion/men/articles_controller.rb
@@ -1,8 +1,10 @@
 class Fashion::Men::ArticlesController < ApplicationController
 
-   def index
+  def index
     if params[:query]
       @article = Article.search(params[:query]).where(category: "mfashion").page(params[:page])
+      flash.now[:notice] = "There are no men's trends matching your search term." if @article.empty?
+      @article = Article.where(category: "mfashion").page(params[:page]) if @article.empty?
     else
       @article = Article.where(category: "mfashion").page(params[:page])
     end
--- app/controllers/fashion/women/articles_controller.rb
+++ app/controllers/fashion/women/articles_controller.rb
@@ -3,6 +3,8 @@ class Fashion::Women::ArticlesController < ApplicationController
   def index
     if params[:query]
       @article = Article.search(params[:query]).where(category: "wfashion").page(params[:page])
+      flash.now[:notice] = "There are no women's trends matching your search term." if @article.empty?
+      @article = Article.where(category: "wfashion").page(params[:page]) if @article.empty?
     else
       @article = Article.where(category: "wfashion").page(params[:page])
     end

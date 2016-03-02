793e2b77f271f75eb671cc5a82512e1020335644
nfamula <nfamula@gmail.com>
2016-03-02 02:46:41 +0000

    bug fix mens search

--- app/controllers/fashion/men/articles_controller.rb
+++ app/controllers/fashion/men/articles_controller.rb
@@ -2,7 +2,7 @@ class Fashion::Men::ArticlesController < ApplicationController
 
    def index
     if params[:query]
-      @article = Article.search(params[:query]).where(category: "wfashion").page(params[:page])
+      @article = Article.search(params[:query]).where(category: "mfashion").page(params[:page])
     else
       @article = Article.where(category: "mfashion").page(params[:page])
     end
